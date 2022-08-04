b134093838870a6c275e4d5e41d3fae1be9633bf
Cyrillo46 <yahooisbetter46@gmail.com>
2022-08-04 19:12:57 -0400

    added photo credits

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ fetch(
     console.log(data);
     console.log(data.urls.full);
     document.body.style.backgroundImage = `url('${data.urls.full}')`;
+    document.getElementById(
+      "photo-credit"
+    ).textContent = `Taken by ${data.user.name}`;
   });
