50a06a6e396b85e90e7325da3a1dcc56975f47a8
Cyrillo46 <yahooisbetter46@gmail.com>
2022-08-03 21:30:51 -0400

    fetched random backgounds

---
+++ app.js
@@ -0,0 +1,8 @@
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.urls.full);
+    document.body.style.backgroundImage = `url('${data.urls.full}')`;
+  });
