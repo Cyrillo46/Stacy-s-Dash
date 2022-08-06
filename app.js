cb28eb5aef918b72f445234d501de00b98225af4
Cyrillo46 <yahooisbetter46@gmail.com>
2022-08-05 23:56:39 -0400

    bitcoin in USD is now displayed

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ fetch(
       "photo-credit"
     ).textContent = `Taken by ${data.user.name}`;
   });
+
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+  .then((res) => res.json())
+  .then((data) => {
+    document.getElementById(
+      "crypto"
+    ).innerHTML = `Bitcoin: ${data.market_data.current_price.usd}`;
+  });
