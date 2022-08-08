e8487646e3cfad353f407a410330df5491459b95
Cyrillo46 <yahooisbetter46@gmail.com>
2022-08-08 11:21:07 -0400

    Time now updates every second

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// fetch our background image
+// fetch our background image for the dashboard and set it
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
 )
@@ -12,7 +12,7 @@ fetch(
     ).textContent = `Taken by ${data.user.name}`;
   });
 
-// display our bitcoin value in USD
+// display our bitcoin value in USD in "crypto-top", an list the status of that currency below in "crypto"
 fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
   .then((res) => res.json())
   .then((data) => {
@@ -21,15 +21,19 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
     ).innerHTML = `<img src="${data.image.small}"> 
     <span>${data.name}</span>`;
 
-    document.getElementById("crypto-top").innerHTML += `
+    document.getElementById("crypto").innerHTML += `
        <p>🎯 Current: ${data.market_data.current_price.usd}</p>
        <p>📈 High: ${data.market_data.high_24h.usd}</p>
        <p>📉 Low: ${data.market_data.low_24h.usd}</p>
     `;
   });
 
-// Our time display
-let time = new Date().toLocaleTimeString(("en-us", { timeStyle: "short" }));
-document.getElementsByClassName("time")[0].innerHTML = time;
+// gets the current time
+function getCurrentTime() {
+  let time = new Date();
+  document.getElementsByClassName("time")[0].innerHTML =
+    time.toLocaleTimeString(("en-us", { timeStyle: "short" }));
+}
 
-console.log(time);
+// updates the current time every second
+setInterval(getCurrentTime, 1000);
